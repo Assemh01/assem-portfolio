29db568b20f852368f85a4df4411f49e615d5fb5
Assemh01 <assem.h2001@gmail.com>
2026-02-11 16:48:32 -0500

    Added google verification

--- pages/index.js
+++ pages/index.js
@@ -65,6 +65,7 @@ export default function Home() {
       <Head>
         <title>Assem Alhomsi | AI Engineer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="HGdQzstagAvp2o57jjBvXR1w6dJFiFt-lbtL37b4u7I" />
 
         <meta
           name="description"
